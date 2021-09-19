91bc4a8b2f641fff407e6b4093bbb23affae4c97
Michael <mikemason1987@gmail.com>
2021-09-18 20:37:21 -0400

    issues with prettier in linux, going back to windows :(

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,79 +1,88 @@
-import asyncHandler from 'express-async-handler'
-import Order from '../models/orderModel.js'
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
 
 //@description  Create new Order
 //@route  POST  /api/orders
 //@acces  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-	const {
-		orderItems,
-		shippingAddress,
-		paymentMethod,
-		itemsPrice,
-		taxPrice,
-		shippingPrice,
-		totalPrice,
-	} = req.body
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
-	if (orderItems && orderItems.length === 0) {
-		res.status(400)
-		throw new Error('No order items')
-	} else {
-		const order = new Order({
-			orderItems,
-			user: req.user._id,
-			shippingAddress,
-			paymentMethod,
-			itemsPrice,
-			taxPrice,
-			shippingPrice,
-			totalPrice,
-		})
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-		const createdOrder = await order.save()
-		res.status(201).json(createdOrder)
-	}
-})
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
 
 //@description  Get order by ID
 //@route  Get  /api/orders/:id
 //@acces  Private
 const getOrderById = asyncHandler(async (req, res) => {
-	const order = await Order.findById(req.params.id).populate(
-		'user',
-		'name email'
-	)
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
-	if (order) {
-		res.json(order)
-	} else {
-		res.status(404)
-		throw new Error('order not found')
-	}
-})
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("order not found");
+  }
+});
 
 //@description  Update Order to paid
 //@route  Get  /api/orders/:id/pay
 //@acces  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-	const order = await Order.findById(req.params.id)
+  const order = await Order.findById(req.params.id);
 
-	if (order) {
-		order.isPaid = true
-		order.paidAt = Date.now()
-		order.paymentResult = {
-			id: req.body.id,
-			status: req.body.status,
-			update_time: req.body.update_time,
-			email_address: req.body.payer.email_address,
-		}
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
 
-		const updatedOrder = await order.save()
-		res.json(updatedOrder)
-	} else {
-		res.status(404)
-		throw new Error('order not found')
-	}
-})
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("order not found");
+  }
+});
 
-export { addOrderItems, getOrderById, updateOrderToPaid }
+//@description  Get uselogged in user orders
+//@route  Get  /api/orders/myorders
+//@acces  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const order = await Order.find({ user: req.user.id });
+
+  res.json(order);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
