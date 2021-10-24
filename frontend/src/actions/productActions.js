b041f0154ca2f2cd5f472ef0c611bb09771c06dd
Abernachy <mikemason1987@gmail.com>
2021-10-23 21:39:18 -0400

    created reviews and added them to the front end

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -123,3 +123,32 @@ export const updateProduct = (prod) => async (dispatch, getState) => {
 		})
 	}
 }
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: product.PRODUCT_CREATE_REVIEW_REQUEST,
+		})
+
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+		await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+		dispatch({
+			type: product.PRODUCT_CREATE_REVIEW_SUCCESS,
+		})
+	} catch (error) {
+		dispatch({
+			type: product.PRODUCT_CREATE_REVIEW_FAIL,
+			payload:
+				error.response && error.response.data.message ? error.response.data.message : error.message,
+		})
+	}
+}
