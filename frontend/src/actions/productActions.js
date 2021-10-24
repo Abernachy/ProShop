c85b325684de7aa9c24c7de2aa66bec5b72ddcb7
Abernachy <mikemason1987@gmail.com>
2021-10-24 15:04:07 -0400

    completed search functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,22 +2,26 @@ import axios from 'axios'
 
 import * as product from '../constants/productConstants'
 
-export const listProducts = () => async (dispatch) => {
-	try {
-		dispatch({ type: product.PRODUCT_LIST_REQUEST })
-		const { data } = await axios.get('/api/products')
-		dispatch({
-			type: product.PRODUCT_LIST_SUCCESS,
-			payload: data,
-		})
-	} catch (error) {
-		dispatch({
-			type: product.PRODUCT_LIST_FAIL,
-			payload:
-				error.response && error.response.data.message ? error.response.data.message : error.message,
-		})
+export const listProducts =
+	(keyword = '') =>
+	async (dispatch) => {
+		try {
+			dispatch({ type: product.PRODUCT_LIST_REQUEST })
+			const { data } = await axios.get(`/api/products?keyword=${keyword}`)
+			dispatch({
+				type: product.PRODUCT_LIST_SUCCESS,
+				payload: data,
+			})
+		} catch (error) {
+			dispatch({
+				type: product.PRODUCT_LIST_FAIL,
+				payload:
+					error.response && error.response.data.message
+						? error.response.data.message
+						: error.message,
+			})
+		}
 	}
-}
 export const listProductDetails = (id) => async (dispatch) => {
 	try {
 		dispatch({ type: product.PRODUCT_DETAILS_REQUEST })
